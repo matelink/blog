d751a05ebb54fdc39e595521c6224312546bdf37
qwertyqqqqq <33512654+qwertyqqqqq@users.noreply.github.com>
2019-03-30 16:01:53 +0700

    Add files via upload

--- Blog/php/db.php
+++ Blog/php/db.php
@@ -1,9 +1,9 @@
 <?php
 global $link;
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', '192.168.1.12');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'kek');
+define('DB_NAME', 'testbd');
 $link = @new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 if ($link == false)
 {
